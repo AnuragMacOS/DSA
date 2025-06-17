97b44172209327bb9bd4667b521c04fd995725e7
Anurag5 <anuraghere555@gmail.com>
2025-06-17 23:42:05 +0530

    Add files via upload

---
+++ LinkedList/DLL_InsertB4tail.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//structure of Node
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* back;
+    
+    public:
+    Node(int data1,Node* next1,Node* back1){
+
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+     }
+   };
+
+    Node* converArr2DLL(vector <int> &arr){
+        Node* head = new Node(arr[0]);
+        Node*prev = head;
+        for(int i=1;i<arr.size();i++){
+            Node* temp= new Node(arr[i],nullptr,prev);
+                prev->next=temp;
+                prev=temp;
+            }
+            return head;
+        }
+    void print(Node* head){
+        while(head!=NULL){
+            cout<< head->data << " ";
+            head = head->next;
+        }
+    }  
+   Node* insertbeforehead(Node* head,int val){
+    Node* newHead = new Node(val,head,nullptr);
+    head->back =newHead;
+     
+    return newHead;
+  }
+  Node* insertbeforetail(Node* head,int val){
+    if(head->next == NULL){
+        return insertbeforehead(head,val);
+    }
+    Node* tail = head;
+    while (tail->next != NULL)
+    {
+        tail= tail->next;
+    }
+    Node* prev = tail->back;
+    Node* newNode = new Node(val,tail,prev);
+    prev->next = newNode;
+    tail->back= newNode;
+    return head;
+  }
+    
+    int main(){
+      vector<int> arr= {12,5,8,7};
+      Node* head= converArr2DLL(arr);
+      head= insertbeforetail(head,100);
+      print (head);
+      return 0;
+   }
+    
