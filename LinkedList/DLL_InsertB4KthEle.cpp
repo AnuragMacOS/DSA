33a477b2a51677a50be7a8786dfa8f5aeef75ee7
Anurag5 <anuraghere555@gmail.com>
2025-06-17 23:41:53 +0530

    Add files via upload

---
+++ LinkedList/DLL_InsertB4KthEle.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//structure of Node
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* back;
+    
+    public:
+    Node(int data1,Node* next1,Node* back1){
+
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+     }
+   };
+
+    Node* converArr2DLL(vector <int> &arr){
+        Node* head = new Node(arr[0]);
+        Node*prev = head;
+        for(int i=1;i<arr.size();i++){
+            Node* temp= new Node(arr[i],nullptr,prev);
+                prev->next=temp;
+                prev=temp;
+            }
+            return head;
+        }
+    void print(Node* head){
+        while(head!=NULL){
+            cout<< head->data << " ";
+            head = head->next;
+        }
+    }  
+   Node* insertbeforehead(Node* head,int val){
+    Node* newHead = new Node(val,head,nullptr);
+    head->back =newHead;
+     
+    return newHead;
+  }
+  
+  Node *InsertbeforeKthEle(Node* head,int k,int val){
+    if(k==1){
+        return insertbeforehead(head,val);
+    }
+    Node* temp = head;
+    int cnt=0;
+    while(temp != NULL){
+        cnt++;
+        if(cnt ==k) break;
+        temp= temp->next;
+    }
+    Node* prev= temp->back;
+    Node* newNode = new Node(val,temp,prev);
+    prev->next = newNode;
+    temp->back = newNode;
+    return head;
+  }
+    
+    int main(){
+      vector<int> arr= {12,5,8,7};
+      Node* head= converArr2DLL(arr);
+      head= InsertbeforeKthEle(head,4,10);
+      print (head);
+      return 0;
+   }
+    
