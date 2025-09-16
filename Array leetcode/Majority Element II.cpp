ce1e1d9747ac74d0de4c9e013590b9e5a3ee6095
Anurag5 <anuraghere555@gmail.com>
2025-09-16 23:48:19 +0530

    Add files via upload

---
+++ Array leetcode/Majority Element II.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& arr) {
+        int cnt1=0,cnt2=0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
+        int n= arr.size();
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && el2!=arr[i]){
+                cnt1=1;
+                el1=arr[i];
+            }
+            else if(cnt2==0 && el1!=arr[i]){
+                cnt2=1;
+                el2=arr[i];
+            }
+            else if (el1==arr[i]) cnt1++;
+            else if (el2==arr[i]) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+         //mannual check on el1 and el2
+         vector<int>ls;
+         cnt1=0;
+         cnt2=0;
+         for(int i=0;i<n;i++){
+            if(el1==arr[i]) cnt1++;
+            if(el2==arr[i]) cnt2++;
+            }
+            int mini=int(n/3)+1;
+            if(cnt1>=mini) ls.push_back(el1);
+            if(cnt2>=mini) ls.push_back(el2);
+            sort(ls.begin(),ls.end());
+            return ls;
+    }
+};
