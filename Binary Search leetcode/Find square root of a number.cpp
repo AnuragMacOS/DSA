e0a2f94c9243f963ced8d02d82276e1c237b272c
Anurag5 <anurag_23it183@dtu.ac.in>
2026-02-24 23:49:11 +0530

    Add files via upload

---
+++ Binary Search leetcode/Find square root of a number.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int floorSqrt(int n)
+    {
+        int low = 1, high = n, ans = 1;
+
+        while (low <= high)
+        {
+            long long mid = (low + high) / 2;
+            long long val = mid * mid;
+            if (val <= n)
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            else
+                high = mid - 1;
+        }
+        return ans;
+    }
+};
