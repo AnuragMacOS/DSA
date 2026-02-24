4d55db1543bbfa9a71b50e3f3a22755b4326f002
Anurag5 <anurag_23it183@dtu.ac.in>
2026-02-24 23:48:41 +0530

    Add files via upload

---
+++ Binary Search leetcode/Find Nth root of a number.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    // ans=ans*mid
+
+    int fun(int mid, int n, int m)
+    {
+        long long ans = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            ans = ans * mid;
+            if (ans > m)
+                return 2;
+        }
+        if (ans == m)
+            return 1;
+        else
+            return 0;
+    }
+
+    int nthRoot(int n, int m)
+    {
+        int low = 1, high = m;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int midN = fun(mid, n, m);
+
+            if (midN == 1)
+            {
+                return mid;
+            }
+            else if (midN == 2)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+};
