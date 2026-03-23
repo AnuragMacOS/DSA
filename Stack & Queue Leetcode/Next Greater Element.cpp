1fa094cf364d2edce2ad55b2d636f082eaabbdc8
Anurag5 <anurag_23it183@dtu.ac.in>
2026-03-23 23:38:09 +0530

    Add files via upload

---
+++ Stack & Queue Leetcode/Next Greater Element.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+
+        unordered_map<int, int> NGEmap;
+        stack<int> st;
+        int n = nums2.size();
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            while (!st.empty() && st.top() <= nums2[i])
+            {
+                st.pop();
+            }
+            if (st.empty())
+                NGEmap[nums2[i]] = -1;
+            else
+                NGEmap[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            ans.push_back(NGEmap[nums1[i]]);
+        }
+        return ans;
+    }
+};
