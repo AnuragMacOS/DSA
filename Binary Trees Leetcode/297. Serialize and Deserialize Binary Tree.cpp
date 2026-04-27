503a41c2b2af50806900750c0d2453069423cf5b
Anurag5 <anurag_23it183@dtu.ac.in>
2026-04-27 23:47:49 +0530

    Add files via upload

---
+++ Binary Trees Leetcode/297. Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,76 @@
+297. Serialize and Deserialize Binary Tree
+
+    class Codec
+{
+public:
+  // Encodes a tree to a single string.
+  string serialize(TreeNode *root)
+  {
+    if (!root)
+      return "";
+    string s = "";
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+      TreeNode *currNode = q.front();
+      q.pop();
+
+      if (currNode == NULL)
+        s.append("#,");
+      else
+        s.append(to_string(currNode->val) + ',');
+
+      if (currNode != NULL)
+      {
+        q.push(currNode->left);
+        q.push(currNode->right);
+      }
+    }
+    return s;
+  }
+
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(string data)
+  {
+    if (data.size() == 0)
+      return NULL;
+    stringstream s(data);
+    string str;
+    getline(s, str, ',');
+    TreeNode *root = new TreeNode(stoi(str));
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+      TreeNode *node = q.front();
+      q.pop();
+      // deserializing left Node
+      getline(s, str, ',');
+      if (str == "#")
+      {
+        node->left = NULL;
+      }
+      else
+      {
+        TreeNode *leftNode = new TreeNode(stoi(str)); // stoi->string to input
+        node->left = leftNode;
+        q.push(leftNode);
+      }
+
+      // deserializing right Node
+      getline(s, str, ',');
+      if (str == "#")
+      {
+        node->right = NULL;
+      }
+      else
+      {
+        TreeNode *rightNode = new TreeNode(stoi(str));
+        node->right = rightNode;
+        q.push(rightNode);
+      }
+    }
+    return root;
+  }
+};
