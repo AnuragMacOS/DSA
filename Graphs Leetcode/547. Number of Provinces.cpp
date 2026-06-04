2627016f44d4eea50e1f092431bd266dc3db9f36
Anurag5 <anurag_23it183@dtu.ac.in>
2026-06-04 22:51:05 +0530

    Add files via upload

---
+++ Graphs Leetcode/547. Number of Provinces.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+private:
+  void dfs(int node, vector<vector<int>> &adj, vector<int> &vis)
+  {
+    vis[node] = 1;
+
+    for (int j = 0; j < adj.size(); j++)
+    {
+      if (adj[node][j] == 1 && !vis[j])
+      {
+        dfs(j, adj, vis);
+      }
+    }
+  }
+
+public:
+  int findCircleNum(vector<vector<int>> &isConnected)
+  {
+    int V = isConnected.size();
+    vector<int> vis(V, 0);
+    int cnt = 0;
+
+    for (int i = 0; i < V; i++)
+    {
+      if (!vis[i])
+      {
+        cnt++;
+        dfs(i, isConnected, vis);
+      }
+    }
+    return cnt;
+  }
+};
