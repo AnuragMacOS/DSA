78810d7a84ef2cfc289c8049393b9a7fc6e96c22
Anurag5 <anurag_23it183@dtu.ac.in>
2026-06-05 17:48:05 +0530

    Add files via upload

---
+++ Graphs Leetcode/Cycle Detection in Undirected Graph (bfs).cpp
@@ -0,0 +1,56 @@
+class Solution
+{
+private:
+  bool detect(int src, vector<vector<int>> &adj, vector<int> &vis)
+  {
+    vis[src] = 1;
+    queue<pair<int, int>> q;
+    q.push({src, -1});
+
+    while (!q.empty())
+    {
+      int node = q.front().first;
+      int parent = q.front().second;
+      q.pop();
+
+      for (auto adjacentNode : adj[node])
+      {
+        if (!vis[adjacentNode])
+        {
+          vis[adjacentNode] = 1;
+          q.push({adjacentNode, node});
+        }
+        // if some node is visited and it did not come from it,
+        // then its not a parent hence becoming a cycle.
+        else if (parent != adjacentNode)
+        {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+public:
+  bool isCycle(int V, vector<vector<int>> &edges)
+  {
+    vector<vector<int>> adj(V);
+    for (auto &edge : edges)
+    {
+      int u = edge[0];
+      int v = edge[1];
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+    }
+    vector<int> vis(V, 0);
+    for (int i = 0; i < V; i++)
+    {
+      if (!vis[i])
+      {
+        if (detect(i, adj, vis))
+          return true;
+      }
+    }
+    return false;
+  }
+};
