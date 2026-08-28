7398bca6cb2a6ba423dfc5913ec44cee291a3932
Anurag5 <anurag_23it183@dtu.ac.in>
2026-08-28 22:48:05 +0545

    Add files via upload

---
+++ Dynamic Programming/213. House Robber II.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+  long long int function(vector<int> &nums)
+  {
+    int n = nums.size();
+    int prev = nums[0];
+    int prev2 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+      int take = nums[i];
+      if (i > 1)
+        take += prev2;
+      int nonTake = 0 + prev;
+
+      int curr_i = max(take, nonTake);
+      prev2 = prev;
+      prev = curr_i;
+    }
+    return prev;
+  }
+
+  int rob(vector<int> &nums)
+  {
+    vector<int> temp1, temp2;
+    int n = nums.size();
+    if (n == 1)
+      return nums[0];
+
+    for (int i = 0; i < n; i++)
+    {
+
+      if (i != 0)
+        temp1.push_back(nums[i]);
+
+      if (i != n - 1)
+        temp2.push_back(nums[i]);
+    }
+    return max(function(temp1), function(temp2));
+  }
+};
+
