e0972b37219e630e4f597ccb03d19b13e13f00b8
Anurag5 <anurag_23it183@dtu.ac.in>
2026-09-22 21:47:36 +0530

    Add files via upload

---
+++ Array leetcode/Spiral Matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int left=0,right=m-1;
+        int top=0,bottom=n-1;
+        vector<int> ans;
+        
+        while(left<=right && top<=bottom){
+        for(int i=left;i<=right;i++){
+            ans.push_back(mat[top][i]);
+        }
+        top++;
+         for(int i=top;i<=bottom;i++){
+            ans.push_back(mat[i][right]);
+        }
+        right--;
+
+        if(top<=bottom){// if top has moved past bottom,
+        // it means we have already processed all   rows,
+        // and we should not proceed further.
+         for(int i=right;i>=left;i--){
+            ans.push_back(mat[bottom][i]);
+        }
+        bottom--;
+        }  
+
+        if (left<=right){
+         for(int i=bottom;i>=top;i--){
+           ans.push_back(mat[i][left]);
+        }
+        left++;
+        }
+
+        }
+        return ans;
+    }
+};
